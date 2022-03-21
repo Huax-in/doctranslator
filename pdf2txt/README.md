54f09833e448c04257feae741a7e215dad81f3ce
Huax-in <huaxcd@gmail.com>
2022-03-21 22:37:03 +0800

    create

---
+++ pdf2txt/README.md
@@ -0,0 +1,1 @@
+Convert pdf file to txt file
---
+++ pdf2txt/app.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+
+	"github.com/unidoc/unipdf/v3/common/license"
+	"github.com/unidoc/unipdf/v3/extractor"
+	pdf "github.com/unidoc/unipdf/v3/model"
+)
+
+func init() {
+	data, err := os.ReadFile("license.txt")
+	if err != nil {
+		panic(err)
+	}
+	// To get your free API key for metered license, sign up on: https://cloud.unidoc.io
+	// Make sure to be using UniPDF v3.19.1 or newer for Metered API key support.
+	err = license.SetMeteredKey(string(data))
+	if err != nil {
+		fmt.Printf("ERROR: Failed to set metered key: %v\n", err)
+		fmt.Printf("Make sure to get a valid key from https://cloud.unidoc.io\n")
+		panic(err)
+	}
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: go run pdf_extract_text.go input.pdf\n")
+		os.Exit(1)
+	}
+
+	// Make sure to enter a valid license key.
+	// Otherwise text is truncated and a watermark added to the text.
+	// License keys are available via: https://unidoc.io
+	/*
+			license.SetLicenseKey(`
+		-----BEGIN UNIDOC LICENSE KEY-----
+		...key contents...
+		-----END UNIDOC LICENSE KEY-----
+		`)
+	*/
+
+	// For debugging.
+	// common.SetLogger(common.NewConsoleLogger(common.LogLevelDebug))
+
+	inputPath := os.Args[1]
+
+	err := outputPdfText(inputPath)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+// outputPdfText prints out contents of PDF file to stdout.
+func outputPdfText(inputPath string) error {
+	f, err := os.Open(inputPath)
+	if err != nil {
+		return err
+	}
+
+	defer f.Close()
+
+	pdfReader, err := pdf.NewPdfReader(f)
+	if err != nil {
+		return err
+	}
+
+	numPages, err := pdfReader.GetNumPages()
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("--------------------\n")
+	fmt.Printf("PDF to text extraction:\n")
+	fmt.Printf("--------------------\n")
+	var data = []byte{}
+	for i := 0; i < numPages; i++ {
+		pageNum := i + 1
+
+		page, err := pdfReader.GetPage(pageNum)
+		if err != nil {
+			return err
+		}
+
+		ex, err := extractor.New(page)
+		if err != nil {
+			return err
+		}
+
+		text, err := ex.ExtractText()
+		if err != nil {
+			return err
+		}
+
+		fmt.Println("------------------------------")
+		fmt.Printf("Page %d:\n", pageNum)
+		data = append(data, []byte("Page "+strconv.Itoa(pageNum)+"\n")...)
+		data = append(data, []byte(text)...)
+		fmt.Printf("\"%s\"\n", text)
+		fmt.Println("------------------------------")
+	}
+	ioutil.WriteFile("out.txt", data, 0600)
+
+	return nil
+}
